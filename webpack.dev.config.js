7f34dec4a75e4e36d16fccfc799ff9a4367e2e7e
danielpqe <danielpqe@gmail.com>
2018-11-29 11:33:31 -0500

    initial config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
   entry: {
     "home": path.resolve(__dirname, 'src/entries/home.js'),
+    "redux": path.resolve(__dirname, 'src/entries/redux.js'),
+
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
